9bc6704b2626e4f493221d8ecb144b69b0b7d7b6
Can Ali Ateş <canaliatess@gmail.com>
2022-08-08 14:49:59 +0300

    Projects

--- Week3/Contour/main.py
+++ Week3/Contour/main.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-image = cv.imread("Iphone.jpg")
+image = cv.imread("ellipse.png")
 cv.waitKey(0)
 
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -11,7 +11,14 @@ cv.waitKey(0)
 contours, hierarchy = cv.findContours(edged, 
                                       cv.RETR_EXTERNAL, 
                                       cv.CHAIN_APPROX_NONE)
-
+""" Multiple Shape/Objects (ellipse.png)
+for count, contour in enumerate(contours):
+    x,y,w,h= cv.boundingRect(contour)
+    croped_image = image[y:(y+h), x:(x+w)]
+    cv.imwrite(f'Ellipse {count + 1}.jpg', croped_image)
+    cv.waitKey(0)
+"""
+    
 cv.drawContours(image, contours, -1, (0, 0, 255), 7)
   
 cv.imshow('Contours', image)
